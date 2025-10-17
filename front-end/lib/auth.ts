6900bd3f1323e67fc603496d3b67ac39495e8bf5
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-10-17 17:54:06 -0300

    chore: Adiciona Batter Auth como gerenciador de autenticação

---
+++ front-end/lib/auth.ts
@@ -0,0 +1,13 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { prisma } from './prisma';
+import { username } from 'better-auth/plugins';
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "mysql"
+    }),
+    plugins: [
+        username()
+    ],
+});
