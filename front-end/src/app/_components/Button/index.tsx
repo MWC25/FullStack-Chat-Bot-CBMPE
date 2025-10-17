2add5eee3eb57454b174ef6c02c578ac3a9199e9
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-10-17 18:00:57 -0300

    feat: Cria o Componente Button

---
+++ front-end/src/app/_components/Button/index.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import { clsx } from 'clsx';
+
+type ButtonProps = {
+    outline?: boolean;
+    children?: ReactNode;
+    className?: string;
+    full?: boolean;
+};
+
+export default function Button({outline = false, children, className, full}: ButtonProps) {
+    const style = clsx(
+        className,
+        outline && 'border-1 bg-transparent',
+        outline === false && 'border-0',
+        full && 'w-full'
+
+    );
+    return (
+        <button
+            className={`${style} font-body-2 bold text-white py-8 px-16 rounded-lg`}>
+            {children}
+        </button>
+    );
+}
