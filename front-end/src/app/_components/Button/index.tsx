5ac2e6996ce0f21e08314e2879e0b23556514835
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-10-23 10:41:04 -0300

    refactor: Refatora o componente Button Adiciona o handler onClick

--- front-end/src/app/_components/Button/index.tsx
+++ front-end/src/app/_components/Button/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { MouseEventHandler, ReactNode } from 'react';
 import { clsx } from 'clsx';
 
 type ButtonProps = {
@@ -6,18 +6,25 @@ type ButtonProps = {
     children?: ReactNode;
     className?: string;
     full?: boolean;
+    onClick?: MouseEventHandler
 };
 
-export default function Button({outline = false, children, className, full}: ButtonProps) {
+export default function Button({
+    outline = false,
+    children,
+    className,
+    full,
+    onClick,
+}: ButtonProps) {
     const style = clsx(
         className,
         outline && 'border-1 bg-transparent',
         outline === false && 'border-0',
         full && 'w-full'
-
     );
     return (
         <button
+            onClick={onClick}
             className={`${style} font-body-2 bold text-white py-8 px-16 rounded-lg`}>
             {children}
         </button>
