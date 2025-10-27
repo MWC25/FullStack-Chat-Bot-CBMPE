5cb659e213610d78cfcebbbc80c065137724d64b
matheushenrique6 <matheushenriquecostaxavier6@gmail.com>
2025-10-27 16:04:23 -0300

    feat: Cria modelo 'Business' para gerenciar informações de negócios

---
+++ Back-end/app/Models/Business.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Business extends Model
+{
+    /** @use HasFactory<\Database\Factories\BusinessFactory> */
+    use HasFactory;
+    protected $table = 'businesses';
+     protected $fillable = ['name_business', 'cnpj'];
+     
+};
