f0901034dfdfb9fcec11dc28978d28c0efdff980
matheushenrique6 <matheushenriquecostaxavier6@gmail.com>
2025-10-27 17:45:19 -0300

    feat: Adiciona configuração e documentação para L5-Swagger

---
+++ Back-end/app/Http/Controllers/SwaggerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * @OA\Info(
+ *     title="API - Matheus Henrique",
+ *     version="1.0.0",
+ *     description="Documentação gerada automaticamente com L5-Swagger."
+ * )
+ *
+ * @OA\Server(
+ *     url="http://localhost:8000",
+ *     description="Servidor local"
+ * )
+ */
+class SwaggerController extends Controller
+{
+    //
+}
