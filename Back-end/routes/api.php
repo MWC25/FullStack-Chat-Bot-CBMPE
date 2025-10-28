6daa20f6055f85ccfef1d13c2aa58c9ec6c30f45
matheushenrique6 <matheushenriquecostaxavier6@gmail.com>
2025-10-28 09:05:46 -0300

    feat: Adiciona rotas CRUD para gerenciamento de usuários no arquivo api.php

--- Back-end/routes/api.php
+++ Back-end/routes/api.php
@@ -13,3 +13,11 @@ Route::post('/createbusiness', [BusinessController::class, 'createBusiness']);
 Route::put('/updatebusiness/{id}', [BusinessController::class, 'updateBusiness']);
 Route::delete('/deletebusiness/{id}', [BusinessController::class, 'deleteBusiness']);
 });
+Route::prefix("user")->group(function () {
+    Route::get('/getusers', [App\Http\Controllers\UserController::class, 'getUsers']);
+    Route::post('/createuser', [App\Http\Controllers\UserController::class, 'createUser']);
+    Route::get('/finduser/{user}', [App\Http\Controllers\UserController::class, 'findUser']);
+    Route::put('/edituser/{user}', [App\Http\Controllers\UserController::class, 'editUser']);
+    Route::patch('/toggleactiveuser/{user}', [App\Http\Controllers\UserController::class, 'toggleActiveUser']);
+    Route::delete('/deleteuser/{user}', [App\Http\Controllers\UserController::class, 'deleteUser']);
+});
