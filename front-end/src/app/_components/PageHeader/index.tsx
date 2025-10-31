ad5742625998e1bbaacee8998a7f3873b8b40d64
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-10-31 20:50:29 -0300

    refactor: Altera o estilo do componente PageHeader

--- front-end/src/app/_components/PageHeader/index.tsx
+++ front-end/src/app/_components/PageHeader/index.tsx
@@ -1,7 +1,7 @@
 export default function PageHeader({ children }: { children?: React.ReactNode }) {
 
     return (
-        <header className="sticky z-10 top-0 bg-light dark:bg-dark-secundary py-24 px-32">
+        <header className="sticky z-10 top-0 bg-light dark:bg-dark-secundary py-24 px-32 border-b-2 border-light-secundary dark:border-dark mb-16">
             <h2 className="title-2">{children}</h2>
         </header>
     );
