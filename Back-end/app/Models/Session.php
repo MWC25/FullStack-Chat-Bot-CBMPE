c83b8952cb2446789a2372df6c1ffbd5354380ec
matheushenrique6 <matheushenriquecostaxavier6@gmail.com>
2025-11-01 22:15:54 -0300

    feat: Cria modelo de sessão para gerenciamento de autenticação

---
+++ Back-end/app/Models/Session.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Session extends Model
+{
+    /** @use HasFactory<\Database\Factories\SessionFactory> */
+    use HasFactory;
+    protected $table = 'sessions';
+    protected $fillable = [
+        'created_at',
+        'expires_at',
+        'last_activity_at',
+        'user_id',
+        'revoked',
+        'revoked_at',
+    ];
+
+}
