0209c2073c3c37a97c7949dffa43f1c538eb3bc7
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-11-01 21:11:47 -0300

    feat: Cria o componente SettingRow para exibição de seções configuráveis

---
+++ front-end/src/app/_components/SettingRow/index.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+export interface SettingRowProps {
+    sectionName: string;
+    sectionData: string;
+    auxiliaryText?: string;
+    buttonAction?: () => void;
+}
+
+export default function SettingRow({
+    sectionName,
+    sectionData,
+    auxiliaryText,
+    buttonAction,
+}: SettingRowProps) {
+    return (
+        <div className="flex flex-col gap-16 justify-center h-fit border-b-2 w-300 py-32">
+            <h4 className="title-4">{sectionName}</h4>
+            <p className="font-body">{sectionData}</p>
+            {buttonAction ? (
+                <button
+                    className="tooltip bold text-primary hover:text-hover  flex dark:text-red-500 dark:hover:text-red-300 cursor-pointer"
+                    onClick={() => {
+                        buttonAction();
+                    }}>
+                    {auxiliaryText}
+                </button>
+            ) : (
+                <p className="tooltip bold text-primary dark:text-red-500 ">
+                    {auxiliaryText}
+                </p>
+            )}
+        </div>
+    );
+}
