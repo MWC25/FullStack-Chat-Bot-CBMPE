f3f295941430cec139aaa40c8b1eedbbb45dc3f4
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-11-03 20:51:48 -0300

    refactor: Simplifica a estrutura do componente Profile e melhora a legibilidade

--- front-end/src/app/dashboard/settings/profile/page.tsx
+++ front-end/src/app/dashboard/settings/profile/page.tsx
@@ -4,48 +4,54 @@ import SettingRow from '@/app/_components/SettingRow';
 import { CircleUserRound } from 'lucide-react';
 import Image from 'next/image';
 
-export default function Profile({ children }: { children?: React.ReactNode }) {
+export default function Profile() {
     const photoUser = '';
 
     return (
-        <div className="flex flex-row justify-evenly w-full h-full">
-            <button className="relative cursor-pointer w-fit h-fit opacity-100 hover:opacity-80 dark:opacity-70 dark:hover:opacity-100">
-                {photoUser ? (
-                    <Image
-                        priority
-                        alt="foto de perfil do usuário"
-                        src={photoUser}
-                        width={256}
-                        height={256}
+        <div className="flex flex-col justify-center gap-16 px-32 py-8">
+            <h3 className="title-3 uppercase bold">Perfil do usuário</h3>
+            <div className="flex flex-row justify-evenly w-full h-full">
+                <button className="relative cursor-pointer w-fit h-fit opacity-100 hover:opacity-80 dark:opacity-70 dark:hover:opacity-100">
+                    {photoUser ? (
+                        <Image
+                            priority
+                            alt="foto de perfil do usuário"
+                            src={photoUser}
+                            width={256}
+                            height={256}
+                        />
+                    ) : (
+                        <CircleUserRound size={256} strokeWidth={1} />
+                    )}
+                    <p className="z-0 tooltip bold text-primary dark:text-red-600">
+                        editar foto de perfil
+                    </p>
+                </button>
+                <div>
+                    <SettingRow
+                        sectionName="Nome"
+                        sectionData="Fulano"
+                        auxiliaryText="alterar nome"
+                        buttonAction={() => console.log('alterando nome')}
                     />
-                ) : (
-                    <CircleUserRound size={256} strokeWidth={1} />
-                )}
-                <p className="z-0 tooltip bold text-primary dark:text-red-600">
-                    editar foto de perfil
-                </p>
-            </button>
-            <div>
-                <SettingRow
-                    sectionName="Nome"
-                    sectionData="Fulano"
-                    auxiliaryText="alterar nome"
-                    buttonAction={() => console.log('alterando nome')}
-                />
-                <SettingRow
-                    sectionName="E-mail"
-                    sectionData="fulano@cbmpe.com"
-                    auxiliaryText="gerenciado por CBMPE"
-                />
-                <SettingRow sectionName="Função" sectionData="Atendente" />
-                <SettingRow sectionName="Linguagem" sectionData="português" />
+                    <SettingRow
+                        sectionName="E-mail"
+                        sectionData="fulano@cbmpe.com"
+                        auxiliaryText="gerenciado por CBMPE"
+                    />
+                    <SettingRow sectionName="Função" sectionData="Atendente" />
+                    <SettingRow
+                        sectionName="Linguagem"
+                        sectionData="português"
+                    />
+                </div>
+                <button className="flex flex-col items-start cursor-pointer w-200">
+                    <h4 className="title-4">Sair</h4>
+                    <p className="tooltip bold text-primary dark:text-red-500 ">
+                        sair da conta
+                    </p>
+                </button>
             </div>
-            <button className='flex flex-col items-start'>
-                <h4 className="title-4">Sair</h4>
-                <p className="tooltip bold text-primary dark:text-red-500 ">
-                    sair da conta
-                </p>
-            </button>
         </div>
     );
 }
